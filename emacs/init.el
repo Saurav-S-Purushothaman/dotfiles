ee354c93e02a2c433d7b94568e2f690d4ea67f8b
Saurav S Purushothaman <saurav.purushothaman@unifize.com>
2024-09-19 21:39:17 +0530

    Add more packages
    - clj refactor
    - Roll back to normal fill column indicator

--- emacs/init.el
+++ emacs/init.el
@@ -62,6 +62,17 @@
 ;; Org mode auto header
 (setq org-autolist-mode t)
 
+;; Clj refactor
+(require 'clj-refactor)
+
+(defun my-clojure-mode-hook ()
+  (clj-refactor-mode 1)
+  (yas-minor-mode 1) ; for adding require/use/import statements
+  ;; This choice of keybinding leaves cider-macroexpand-1 unbound
+  (cljr-add-keybindings-with-prefix "C-c C-m"))
+
+(add-hook 'clojure-mode-hook #'my-clojure-mode-hook)
+
 
 ;; font
 ;; (set-frame-font "PT Mono-16")
@@ -120,6 +131,10 @@
  '(fill-column-indicator
    ((t (:foreground "gray30")))))
 
+;; Change the color of the fill column indicator to normal mode
+(custom-set-faces
+ '(fill-column-indicator
+   ((t))))
 
 (defun xah-toggle-line-spacing ()
   "Toggle line spacing between no extra space to extra half line height.
