af246e85d5fb883db9d478072149193a5dfc769f
Saurav S Purushothaman <saurav.purushothaman@unifize.com>
2025-02-10 11:42:29 +0530

    Install straight.el

--- emacs/init.el
+++ emacs/init.el
@@ -319,6 +319,23 @@ Version 2017-06-02"
   :config
   (add-hook 'prog-mode-hook 'adaptive-wrap-prefix-mode))
 
+;; Installing straight.el
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name
+        "straight/repos/straight.el/bootstrap.el"
+        (or (bound-and-true-p straight-base-dir)
+            user-emacs-directory)))
+      (bootstrap-version 7))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
 
 
 
