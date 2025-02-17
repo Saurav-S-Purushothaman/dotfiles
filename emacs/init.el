3fdc6e36a753625c758e6c4040d04a58899bad0c
Saurav S Purushothaman <saurav.purushothaman@unifize.com>
2025-02-17 12:24:37 +0530

    Python mode setup

--- emacs/init.el
+++ emacs/init.el
@@ -319,6 +319,20 @@ Version 2017-06-02"
   :config
   (add-hook 'prog-mode-hook 'adaptive-wrap-prefix-mode))
 
+;; python mode setup
+(use-package pyvenv
+  :ensure t
+  :config
+  (pyvenv-mode t)
+
+  ;; Set correct Python interpreter
+  (setq pyvenv-post-activate-hooks
+        (list (lambda ()
+                (setq python-shell-interpreter (concat pyvenv-virtual-env "bin/python3")))))
+  (setq pyvenv-post-deactivate-hooks
+        (list (lambda ()
+                (setq python-shell-interpreter "python3")))))
+
 ;; Installing straight.el
 (defvar bootstrap-version)
 (let ((bootstrap-file
