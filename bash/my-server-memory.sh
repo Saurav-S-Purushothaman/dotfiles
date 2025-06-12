ecc534c9fd5651cf333312fdc0b5144329a6bfaf
Saurav S Purushothaman <saurav.purushothaman@unifize.com>
2025-06-13 01:06:56 +0530

    No need to store the user information
    The user information we are storing in the usr variable is
    unnecessary. It is not used anywhere else.

--- bash/my-server-memory.sh
+++ bash/my-server-memory.sh
@@ -1,6 +1,6 @@
 # To find the current RAM and Memory usage of my server
 
-read usr pid _ < <(ps aux | grep "java -classpath /Users/sauravspurushothaman/unifize-server/apps/api/test:/Users/sauravspurushothaman/unifize-server/apps/api/src:")
+read _ pid _ < <(ps aux | grep "java -classpath /Users/sauravspurushothaman/unifize-server/apps/api/test:/Users/sauravspurushothaman/unifize-server/apps/api/src:")
 
 read mem rss < <(ps -p $pid -o %mem,rss | tail -n 1)
 declare mb=1024
