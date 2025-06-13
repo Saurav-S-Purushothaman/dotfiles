9784665e2a39b3f1de43dc1c389b1b8f5b36e0d4
Saurav S Purushothaman <saurav.purushothaman@unifize.com>
2025-06-13 11:20:42 +0530

    Add TODO

--- bash/my-server-memory.sh
+++ bash/my-server-memory.sh
@@ -1,5 +1,7 @@
 # To find the current RAM and Memory usage of my server
 
+# TODO: I need to get the PID of my Java server. Maybe, I can make the
+# name of my server an arg instead of hardcoding it here.
 read _ pid _ < <(ps aux | grep "java -classpath /Users/sauravspurushothaman/unifize-server/apps/api/test:/Users/sauravspurushothaman/unifize-server/apps/api/src:")
 
 read mem rss < <(ps -p $pid -o %mem,rss | tail -n 1)
