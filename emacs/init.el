664fddff8d1d1be222072a7bddd30e577a690b86
Saurav S Purushothaman <saurav.purushothaman@unifize.com>
2025-09-02 22:11:00 +0530

    Use ef theme

--- emacs/init.el
+++ emacs/init.el
@@ -747,3 +747,30 @@ by Prelude.")
 
     (fontaine-set-preset (or (fontaine-restore-latest-preset) 'regular))
     (fontaine-mode 1))
+
+
+(use-package ef-themes
+  :ensure t
+  :demand t
+  :bind
+  (("<f5>" . ef-themes-rotate)
+   ("C-<f5>" . ef-themes-select))
+  :config
+  (setq ef-themes-to-toggle '(ef-elea-light ef-elea-dark))
+  (setq ef-themes-variable-pitch-ui t)
+  (setq ef-themes-mixed-fonts t)
+  (setq ef-themes-rotate ef-themes-items)
+  (setq ef-themes-headings      ; read the manual's entry of the doc string
+        '((0 . (variable-pitch light 1.9))
+          (1 . (variable-pitch light 1.8))
+          (2 . (variable-pitch regular 1.7))
+          (3 . (variable-pitch regular 1.6))
+          (4 . (variable-pitch regular 1.5))
+          (5 . (variable-pitch 1.4))  ; absence of weight means `bold'
+          (6 . (variable-pitch 1.3))
+          (7 . (variable-pitch 1.2))
+          (agenda-date . (semilight 1.5))
+          (agenda-structure . (variable-pitch light 1.9))
+          (t . (variable-pitch 1.1))))
+  (setq ef-themes-disable-other-themes t)
+  (mapc #'disable-theme custom-enabled-themes))
